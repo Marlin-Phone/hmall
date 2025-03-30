496dba0e7ad307e61191c9ee7746efca21e3bbe5
Marlin <Marlin_Phone@outlook.com>
2025-03-30 22:50:23 +0800

    feat: 更新 Cart 模块以使用 heima.api 包，整合 Feign 客户端

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.heima.api.client")
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 class CartApplication {
